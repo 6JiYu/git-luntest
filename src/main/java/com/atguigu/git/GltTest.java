8848ec5a0e388a093721bdacc57c75de56a784bb
Jiyu <282628973@qq.com>
2023-03-02 20:10:11 +0800

    second commit

--- src/main/java/com/atguigu/git/GltTest.java
+++ src/main/java/com/atguigu/git/GltTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GltTest {
     public static void main(String[] args) {
         System.out.println("HELLO GIT!");
+        System.out.println("HELLO GIT 2!");
     }
 }
